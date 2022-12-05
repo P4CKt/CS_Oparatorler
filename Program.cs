b115acb4fd472b5e38baf9e077acee2bb593780b
P4CKt <omurcanuslu@outlook.com>
2022-12-05 22:35:33 +0300

    www.patika.dev

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace odev_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // atama
+            int x = 3;
+            int y = 3;
+            y= 
+        }
+    }
+}
