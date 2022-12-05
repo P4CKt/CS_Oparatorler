78b864908a305a559dedb2eb34369cff7e921112
P4CKt <omurcanuslu@outlook.com>
2022-12-06 00:40:22 +0300

    ilk yükleme

--- Program.cs
+++ Program.cs
@@ -9,7 +9,16 @@ namespace odev_2
             // atama
             int x = 3;
             int y = 3;
-            y= 
+            y= y+2;
+            //birleşebilir işlemli atama--> y+=2;
+            Console.WriteLine(y);
+            y+=2;
+            Console.WriteLine(y);
+            y/=1;
+            Console.WriteLine(y);
+            x*=2; //x=x*2
+            Console.WriteLine(x);
+
         }
     }
 }
